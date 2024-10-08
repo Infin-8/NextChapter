b9a35b4af98c73dd796846aeacc7479b78722c54
Nathaniel Vazquez <nathaniel.a.vazquez@gmail.com>
2024-10-07 18:35:56 -0700

    fixed: buget bug

--- Utilities/helpers.mjs
+++ Utilities/helpers.mjs
@@ -5,26 +5,28 @@ export const getSum = arr => arr.reduce((a, b) => a + b, 0)
 
 export const mapAmounts = arr => arr.map(({ amount }) => amount)
 
-export const usdFormatter = str => new Intl
-    .NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        maximumSignificantDigits: 6
-    })
-    .format(str);
+export const usdFormatter = str => str
+    ? new Intl
+        .NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            maximumSignificantDigits: 6
+        })
+        .format(str)
+    : "$0";
 
 export const titleCase = str => str
     .toLowerCase()
     .replace(/^\w|\s\w/g, l => l.toUpperCase())
 
 export const sanitize = input => input
-    .replace(/&/g, '&amp;')    
-    .replace(/</g, '&lt;')     
-    .replace(/>/g, '&gt;')     
-    .replace(/"/g, '&quot;')  
-    .replace(/'/g, '&#39;')  
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
     .replace(/[^a-zA-Z0-9 .,!?@#%&()-_]/g, '')
-    .trim();            
+    .trim();
 
 export const sanitizeTrolls = input => input.slice(0, 8)
 
